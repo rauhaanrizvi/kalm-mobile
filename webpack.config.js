582fafa08248dbe21ba618ce01204e9cf2099dbd
rauhaanrizvi <43570527+rauhaanrizvi@users.noreply.github.com>
2021-11-09 11:13:30 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
+  devServer: {
+      host: '0.0.0.0',
+      allowedHosts: 'all',
+  },
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
   return config;
